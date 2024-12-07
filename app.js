ceccc5595fdf4257190a60247fbaafa24af747f3
tsfmakko123 <acerfp563@msn.com>
2024-12-07 18:41:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("hi")
+// 引入 dotenv 套件
+require('dotenv').config();
+
+// 使用環境變數
+const port = process.env.PORT;
+// const dbHost = process.env.DB_HOST;
+
+// console.log(`App is running on port: ${port}`);
+// console.log(`Database host is: ${dbHost}`);
+consloe.log(process.env.DEPLOY_KEY;)
