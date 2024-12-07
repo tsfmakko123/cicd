57792c4f8912814682dbe31801115530dc7e9932
tsfmakko123 <acerfp563@msn.com>
2024-12-07 20:26:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ require('dotenv').config();
 
 // console.log(`App is running on port: ${port}`);
 // console.log(`Database host is: ${dbHost}`);
-consloe.log(process.env.DEPLOY_KEY;)
+const DEPLOY_KEY=process.env.DEPLOY_KEY
+console.log("DEPLOY_KEY:",DEPLOY_KEY);
 
 
 console.log("執行app.js結束")
+
